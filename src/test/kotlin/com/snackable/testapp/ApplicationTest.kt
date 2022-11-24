fc408a85efac8498d65be115d8cdba888dd73c59
Anni <anni.uutma@guardtime.com>
2022-11-24 22:47:12 +0200

    fix test

--- src/test/kotlin/com/snackable/testapp/ApplicationTest.kt
+++ src/test/kotlin/com/snackable/testapp/ApplicationTest.kt
@@ -1,25 +1,21 @@
 package com.snackable.testapp
 
-import io.ktor.server.plugins.contentnegotiation.ContentNegotiation as ServerContentNegotiation
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation as ClientContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.http.HttpStatusCode
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.call
+import io.ktor.server.application.install
 import io.ktor.server.response.respond
-import io.ktor.server.response.respondOutputStream
-import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import io.ktor.server.testing.testApplication
-import kotlin.test.Ignore
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation as ClientContentNegotiation
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation as ServerContentNegotiation
 
-// sorry this is not working yet
 class ApplicationTest {
     @Test
-    @Ignore
     fun test() = testApplication {
         val client = createClient {
             install(ClientContentNegotiation) {
@@ -27,24 +23,27 @@ class ApplicationTest {
             }
         }
 
+        application {
+            module(client)
+        }
+
         externalServices {
             hosts("http://interview-api.snackable.ai") {
+                install(ServerContentNegotiation) {
+                    json()
+                }
                 routing {
                     get("api/file/all") {
-                        call.respond(File("test", ProcessingStatus.FINISHED))
+                        call.respond(listOf(File("test", ProcessingStatus.FINISHED)))
                     }
                     get("api/file/details/test") {
                         call.respond(Details("test", "name", "mp3", "filepath", 123))
                     }
-                    get("api/file/details/segments/test") {
-                        call.respond(arrayOf(Segment(1, "test", "segment", 2, 3)))
+                    get("api/file/segments/test") {
+                        call.respond(listOf(Segment(1, "test", "segment", 2, 3)))
                     }
                 }
             }
-        }
-
-        application {
-            module(client)
         }
 
         client.get("/api/file/metadata/test").apply {
